3c853cfea630816766c595816be15eaf4cc70039
Malik Kashif <139557529+Kashif5103@users.noreply.github.com>
2023-11-23 15:56:03 +0500

    Add files via upload
    Add the Module 16(b)

---
+++ python/mymedule2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 01:45:09 2023
+
+@author: HC
+"""
+
+import Mymedule as first
+
+#Mymedule.greating('Kashif')
+
+x=first.person['name']
+print(x)
+
+# Build in medule in python
+import platform
+b=dir(platform)
+a=platform.system()
+print(b)
+print(a)
+from Mymedule import person
+print(person['country'])
