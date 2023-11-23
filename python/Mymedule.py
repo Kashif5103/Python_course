00200f9fa828d7ce2331280a21b9d9335f59eeda
Malik Kashif <139557529+Kashif5103@users.noreply.github.com>
2023-11-23 15:55:09 +0500

    Add files via upload
    Add the Moudule 16

---
+++ python/Mymedule.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 01:39:41 2023
+
+@author: HC
+"""
+
+#def greating(name):
+#    print('Hi '+name+" wellcome")
+
+person={
+       'name':'Kashif',
+       'age':22,
+       'country':'Pakistan'
+       }
